24276259b4db94ec4ef6571e151be34f0b17dae5
kpanos <panos.katsoulis@cern.ch>
2019-04-17 09:12:44 +0200

    added functions 'enableSosLinks' and 'disableSosLinks' in the useful_functions file

--- condor_prod/useful_functions.sh
+++ condor_prod/useful_functions.sh
@@ -19,4 +19,39 @@ function cleanProducts(){
     return 0
 }
 
+function enableSosLinks(){
+    [ -e analysis_scripts/sosmca.link.disabled ] && {
+	mv analysis_scripts/sosmca.link.disabled analysis_scripts/sosmca.link
+	mv analysis_scripts/sosmca.txt analysis_scripts/sosmca.txt.disabled
+	mv analysis_scripts/sosmca.link analysis_scripts/sosmca.txt
+    }
+    [ -e analysis_scripts/soscuts.link.disabled ] && {
+	mv analysis_scripts/soscuts.link.disabled analysis_scripts/soscuts.link
+	mv analysis_scripts/soscuts.txt analysis_scripts/soscuts.txt.disabled
+	mv analysis_scripts/soscuts.link analysis_scripts/soscuts.txt
+    }
+    [ -e analysis_scripts/sosplots.link.disabled ] && {
+	mv analysis_scripts/sosplots.link.disabled analysis_scripts/sosplots.link
+	mv analysis_scripts/sosplots.txt analysis_scripts/sosplots.txt.disabled
+	mv analysis_scripts/sosplots.link analysis_scripts/sosplots.txt
+    }
+    return 0
+}
+
+function disableSosLinks(){
+    [ -h analysis_scripts/sosmca.txt ] && {
+	mv analysis_scripts/sosmca.txt analysis_scripts/sosmca.link.disabled
+	mv analysis_scripts/sosmca.txt.disabled analysis_scripts/sosmca.txt
+    }
+    [ -h analysis_scripts/soscuts.txt ] && {
+	mv analysis_scripts/soscuts.txt analysis_scripts/soscuts.link.disabled
+	mv analysis_scripts/soscuts.txt.disabled analysis_scripts/soscuts.txt
+    }
+    [ -h analysis_scripts/sosplots.txt ] && {
+	mv analysis_scripts/sosplots.txt analysis_scripts/sosplots.link.disabled
+	mv analysis_scripts/sosplots.txt.disabled analysis_scripts/sosplots.txt
+    }
+    return 0
+}
+
 printUsefulFunctions
