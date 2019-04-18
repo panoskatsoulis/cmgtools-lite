32dbcead624356315a0fa629a42d7f98b6606b68
kpanos <panos.katsoulis@cern.ch>
2019-04-18 10:47:31 +0200

    changes to the prepare_condor_resubmit script

--- condor_prod/prepare_condor_resubmit.sh
+++ condor_prod/prepare_condor_resubmit.sh
@@ -26,23 +26,25 @@ while [ ! -z $1 ]; do
     echo "Unsupported argument, $1" && exit 101
 done
 
-{ [ -z "$CONDOR_CLUSTER" ] || [ -z "$PROCESS" ] || [ -z "$CONDOR_SUBMIT_FILE" ]; } \
-    && { echo "The cluster-id, the process and the initial condor submit file must be specified."; exit 1; }
 [ -z $FAILURE_SPECIFIC ] && FAILURE_SPECIFIC=false
 [ -z $PROD_FAILED_MODE ] && PROD_FAILED_MODE=false
+if ! $PROD_FAILED_MODE && ! $FAILURE_SPECIFIC; then
+    { [ -z "$CONDOR_CLUSTER" ] || [ -z "$PROCESS" ] || [ -z "$CONDOR_SUBMIT_FILE" ]; } \
+	&& { echo "The cluster-id, the process and the initial condor submit file must be specified."; exit 1; }
 
-{ [ -e ${CONDOR_SUBMIT_FILE}.resubmit ] && [ ! $FAILURE_SPECIFIC ]; } && {
-    printf ".resubmit file for the given condor submit file alrady exist.\nDo you want to proceed and overwrite the old file? [y/n]";
-    read ans;
-    case $ans in
-	y)
-	    echo "will overwrite the old .duplicate file.";;
-	n)
-	    echo "exiting."; exit 0;;
-	*)
-	    echo "unknown answer, exiting."; exit 2;;
-    esac
-}
+    [ -e ${CONDOR_SUBMIT_FILE}.resubmit ] && {
+	printf ".resubmit file for the given condor submit file alrady exist.\nDo you want to proceed and overwrite the old file? [y/n]";
+	read ans;
+	case $ans in
+	    y)
+		echo "will overwrite the old .duplicate file.";;
+	    n)
+		echo "exiting."; exit 0;;
+	    *)
+		echo "unknown answer, exiting."; exit 2;;
+	esac
+    }
+fi
 
 if $FAILURE_SPECIFIC; then
     SUFFIX=$(echo $FAILURE | sed -r 's@[^A-Za-z0-9]+@\-@g')
@@ -58,15 +60,26 @@ if $PROD_FAILED_MODE; then
     ## This mode checks for the flag PRODUCTION_DONE in the out files of the sosTrees_production
     ## if the flag is true comment out the job from the condor submit file
     ## it works with the simple .resubmit file
+    ! ls output/*.out && { echo "No jobs exist in the output/ path yet."; exit 11; }
     for outfile in $(ls output/*.out); do
-	JOB=$(echo $outfile | sed -r 's@.*/hello\.(.*\..*)\.out$@\1@')
-	TREE_PRODUCER=$(find prod_treeProducersPerProcess/ | grep ${JOB}\.py$)
-	[ ! -e $TREE_PRODUCER ] && { echo "Tree producer for the job $JOB couldn't be found"; continue; }
-	FILES=$(grep files.*#sosTrees_production $TREE_PRODUCER | sed -r 's@.*\[(.*):(.*)\].*@\1,\2@')
-	PRODUCTION_DONE=$(grep PRODUCTION_DONE $outfile | sed 's/ //g' | awk -F = '{print $2}')
+	JOB=$(echo $outfile | sed -r 's@.*/hello\.(.*\..*)\.out$@\1@') && {
+	    [ ! -z $JOB ] && echo "job found."; }
+	TREE_PRODUCER=$(find prod_treeProducersPerProcess/ | grep ${JOB}\.py$) && {
+	    [ ! -z $TREE_PRODUCER ] && echo "producer found."; }
+	[ -z $TREE_PRODUCER ] && { echo "Tree producer for the job $JOB couldn't be found"; continue; }
+	FILES=$(grep files.*#sosTrees_production $TREE_PRODUCER | sed -r 's@.*\[(.*):(.*)\].*@\1,\2@') && {
+	    [ ! -z $FILES ] && echo "files found."; }
+	PRODUCTION_DONE=$(grep PRODUCTION_DONE $outfile | sed -r 's/ //g; s/\x1B\[0[;0-9]*m//g;' | awk -F = '{print $2}') && {
+	    [ ! -z $PRODUCTION_DONE ] && echo "flag found."; }
+	[ -z $PRODUCTION_DONE ] && PRODUCTION_DONE=false
 	echo ">> $JOB, $TREE_PRODUCER, $(echo $FILES | tr ',' ':'), $PRODUCTION_DONE)"
 
-	$PRODUCTION_DONE && sed -i -r "s@(^.*${FILES}$)@#\1@" $CONDOR_SUBMIT_FILE
+	echo $PRODUCTION_DONE
+	if $PRODUCTION_DONE; then
+	    sed -i -r "s@(^.*${FILES}$)@#\1@" $CONDOR_SUBMIT_FILE
+	else
+	    rm -f $outfile
+	fi
     done
     exit 0
 fi
