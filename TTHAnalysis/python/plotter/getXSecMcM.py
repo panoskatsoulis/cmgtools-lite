a69be5658ae1728513c3591afda72662287f86b2
kpanos <panos.katsoulis@cern.ch>
2020-01-30 17:15:08 +0100

    das_client.py to dasgoclient

--- TTHAnalysis/python/plotter/getXSecMcM.py
+++ TTHAnalysis/python/plotter/getXSecMcM.py
@@ -3,21 +3,24 @@ from subprocess import check_output
 from sys import argv
 
 def dasql(x):
-    txtjson = check_output(["das_client.py", "--query=%s" %x, "--limit=10000", "--format=json"])
+    txtjson = check_output(["dasgoclient", "--query=%s" %x, "--limit=10000", "--format=json"])
     return json.loads(txtjson)
 
 def go(dataset):
     try:
         dsets = dasql("dataset dataset=%s" % dataset)['data']
+        # print dsets
     except:
         print "Failed DAS query for %s" % dataset
         return
     if "*" in dataset:
         for d in dsets:
             go(d['dataset'][0]['name'])
+        print "A multidataset argument has been passed into the das query."
         return
     for d in dsets:
         for record in d['dataset']:
+            print record
             if 'mcm' in record:
                 mcm = record['mcm']
                 gen = mcm['generator_parameters'][0]
