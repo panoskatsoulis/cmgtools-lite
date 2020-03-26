08cfff7993796bad5c82af9a45970e570ce6dd0e
kpanos <panos.katsoulis@cern.ch>
2020-03-26 13:37:52 +0100

    redirected wrapper into ODIR from sos_plots.py, removed AccountingGroup from condor sub file

--- TTHAnalysis/python/plotter/susy-sos/scripts/htcondor_submitter.sh
+++ TTHAnalysis/python/plotter/susy-sos/scripts/htcondor_submitter.sh
@@ -4,5 +4,4 @@ output          = [DIR]/job.[NAME].0.out
 error           = [DIR]/job.[NAME].0.err
 log             = [DIR]/job.[NAME].log
 +JobFlavour	= "[QUEUE]"
-+AccountingGroup = "group_u_CMST3.all"
 queue
